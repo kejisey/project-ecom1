32964cbb4d3d38a11f3cda03440611eb2e87036f
Damian Matuszewski <kejisey@gmail.com>
2016-01-26 20:27:41 +0100

    6 - Model Managers + Product variations + Post save signals for variations

--- Djangop/src/products/models.py
+++ Djangop/src/products/models.py
@@ -1,7 +1,20 @@
 from django.core.urlresolvers import reverse
 from django.db import models
-
+from django.db.models.signals import post_save
 # Create your models here.
+
+class ProductQuerySet(models.query.QuerySet):
+	def active(self):
+		return self.filter(active=True)
+
+
+class ProductManager(models.Manager):
+	def get_queryset(self):
+		return ProductQuerySet(self.model, using=self._db)
+
+	def all(self, *args, **kwargs):
+		return self.get_queryset().active()
+
 
 
 class Product(models.Model):
@@ -12,6 +25,8 @@ class Product(models.Model):
 	#slug
 	#inventory?
 
+	objects = ProductManager()
+
 
 	def __unicode__(self): #def __str__(self):
 		return self.title 
@@ -19,8 +34,47 @@ class Product(models.Model):
 	def get_absolute_url(self):
 		return reverse("product_detail", kwargs={"pk": self.pk})
 
-# product images
 
 
 
-# product category
+class Variation(models.Model):
+	product = models.ForeignKey(Product)
+	title = models.CharField(max_length=120)
+	price = models.DecimalField(decimal_places=2, max_digits=20)
+	sale_price = models.DecimalField(decimal_places=2, max_digits=20, null=True, blank=True)
+	active = models.BooleanField(default=True)
+	inventory = models.IntegerField(null=True, blank=True) #refer none == unlimited amount
+
+	def __unicode__(self):
+		return self.title
+
+	def get_price(self):
+		if self.sale_price is not None:
+			return self.sale_price
+		else:
+			return self.price
+
+
+	def get_absolute_url(self):
+		return self.product.get_absolute_url()
+
+
+
+def product_post_saved_receiver(sender, instance, created, *args, **kwargs):
+	product = instance
+	variations = product.variation_set.all()
+	if variations.count() == 0:
+		new_var = Variation()
+		new_var.product = product
+		new_var.title = "Default"
+		new_var.price = product.price
+		new_var.save()
+
+
+post_save.connect(product_post_saved_receiver, sender=Product)
+
+
+# Product Images
+
+
+# Product Category
--- Djangop/src/products/views.py
+++ Djangop/src/products/views.py
@@ -1,3 +1,5 @@
+
+
 from django.http import Http404
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
@@ -6,19 +8,29 @@ from django.utils import timezone
 
 # Create your views here.
 
+
 from .models import Product
+
+
 
 class ProductListView(ListView):
 	model = Product
+	queryset = Product.objects.all()
 
 	def get_context_data(self, *args, **kwargs):
 		context = super(ProductListView, self).get_context_data(*args, **kwargs)
 		context["now"] = timezone.now()
 		return context
+
+
 class ProductDetailView(DetailView):
 	model = Product
 	#template_name = "product.html"
 	#template_name = "<appname>/<modelname>_detail.html"
+
+
+
+
 
 def product_detail_view_func(request, id):
 	#product_instance = Product.objects.get(id=id)
