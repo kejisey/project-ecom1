2994ad98d12379c9cb557ab6afb204ad2aa7e96a
Damian Matuszewski <kejisey@gmail.com>
2016-01-26 20:13:39 +0100

    5 - ListView and using links for Model instances

--- Djangop/src/products/models.py
+++ Djangop/src/products/models.py
@@ -1,3 +1,4 @@
+from django.core.urlresolvers import reverse
 from django.db import models
 
 # Create your models here.
@@ -14,3 +15,12 @@ class Product(models.Model):
 
 	def __unicode__(self): #def __str__(self):
 		return self.title 
+
+	def get_absolute_url(self):
+		return reverse("product_detail", kwargs={"pk": self.pk})
+
+# product images
+
+
+
+# product category
--- Djangop/src/products/views.py
+++ Djangop/src/products/views.py
@@ -1,14 +1,24 @@
-from django.views.generic.detail import DetailView
 from django.http import Http404
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
 
+# Create your views here.
 
 from .models import Product
 
-
-class ProductDetailView(DetailView):
+class ProductListView(ListView):
 	model = Product
 
+	def get_context_data(self, *args, **kwargs):
+		context = super(ProductListView, self).get_context_data(*args, **kwargs)
+		context["now"] = timezone.now()
+		return context
+class ProductDetailView(DetailView):
+	model = Product
+	#template_name = "product.html"
+	#template_name = "<appname>/<modelname>_detail.html"
 
 def product_detail_view_func(request, id):
 	#product_instance = Product.objects.get(id=id)
